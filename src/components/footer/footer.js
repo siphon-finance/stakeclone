616879aabc6e1c0a8460c22d572ff6339e6373c3
superbeefyboy <superbeefyboy@pm.me>
2020-10-03 16:23:55 -0300

    proper navigation

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -15,17 +15,17 @@ const Footer = () => {
       <Column>
         <Title>beefy.finance</Title>
 
-        <Link href="https://beefy.finance">
+        <Link href="https://beefy.finance" target="_blank">
           <FAIcon type='globe' />
           <LinkLabel>website</LinkLabel>
         </Link>
 
-        <Link href="https://medium.com/beefyfinance">
+        <Link href="https://medium.com/beefyfinance" target="_blank">
           <BrandIcon type='medium' />
           <LinkLabel>news</LinkLabel>
         </Link>
 
-        <Link href="https://github.com/beefyfinance">
+        <Link href="https://github.com/beefyfinance" target="_blank">
           <BrandIcon type='github' />
           <LinkLabel>source</LinkLabel>
         </Link>
@@ -33,17 +33,17 @@ const Footer = () => {
 
       <Column>
         <Title>products</Title>
-        <Link href="https://gov.beefy.finance">
+        <Link href="https://gov.beefy.finance" target="_blank">
           <FAIcon type='landmark' />
           <LinkLabel>gov</LinkLabel>
         </Link>
 
-        <Link href="https://vote.beefy.finance">
+        <Link href="https://vote.beefy.finance" target="_blank">
           <FAIcon type='vote-yea' />
           <LinkLabel>vote</LinkLabel>
         </Link>
 
-        <Link href="https://app.beefy.finance">
+        <Link href="https://app.beefy.finance" target="_blank">
           <FAIcon type='hand-holding-usd' />
           <LinkLabel>app</LinkLabel>
         </Link>
@@ -51,15 +51,15 @@ const Footer = () => {
 
       <Column>
         <Title>socials</Title>
-        <Link href="https://twitter.com/beefyfinance">
+        <Link href="https://twitter.com/beefyfinance" target="_blank">
           <BrandIcon type='twitter' />
           <LinkLabel>twitter</LinkLabel>
         </Link>
-        <Link href="https://t.me/beefyfinance">
+        <Link href="https://t.me/beefyfinance" target="_blank">
           <BrandIcon type='telegram' />
           <LinkLabel>telegram</LinkLabel>
         </Link>
-        <Link href="https://discord.gg/9xfMvJY">
+        <Link href="https://discord.gg/9xfMvJY" target="_blank">
           <BrandIcon type='discord' />
           <LinkLabel>discord</LinkLabel>
         </Link>
