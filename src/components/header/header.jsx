5068ce43df682c90888710f43e927119c39494d6
superbeefyboy <superbeefyboy@pm.me>
2020-10-03 16:18:45 -0300

    fixed header positions

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -112,7 +112,6 @@ const styles = theme => ({
     border: '1px solid #F8F2EC',
     borderRadius: '0',
     background: '#F8F2EC',
-    marginBottom: '2rem',
     fontWeight: 900,
     color: '#000',
   },
