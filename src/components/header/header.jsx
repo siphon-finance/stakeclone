690b16b9185035b65ab4b066948952063be44d96
superbeefyboy <superbeefyboy@pm.me>
2020-11-19 00:43:04 -0300

    stats link

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -174,6 +174,7 @@ class Header extends Component {
             { this.renderLink('gov', 'gov', 'landmark') }
             { this.renderLink('vote', 'vote', 'vote-yea') }
             { this.renderLink('app', 'app', 'hand-holding-usd') }
+            { this.renderLink('dashboard', 'stats', 'chart-bar')}
             { this.renderLink('docs', 'docs', 'book') }
           </div>
           <div className={classes.account}>
