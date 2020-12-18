c9a9276c7966c6060d1b30591000f576fa7eac1e
superbeefyboy <superbeefyboy@pm.me>
2020-12-18 19:31:03 -0300

    placeholder logo

--- src/stores/store.jsx
+++ src/stores/store.jsx
@@ -69,6 +69,7 @@ class Store {
           tokens: [
             {
               id: 'cake-twt-bnb',
+              logo: 'trust-logo.png',
               address: '0x3b2c93f9455786e74C68648EaaB09B20ce557537',
               symbol: 'mooPancakeTWT-BNB',
               abi: config.erc20ABI,
@@ -96,6 +97,7 @@ class Store {
           tokens: [
             {
               id: 'thugs-drugs-guns',
+              logo: 'thugs-logo.png',
               address: '0x8AE31751A226B0C5357a377E53B6DB12bDF5e64d',
               symbol: 'mooDrugsGunsV2',
               abi: config.erc20ABI,
@@ -123,6 +125,7 @@ class Store {
           tokens: [
             {
               id: 'narwhal-thugs-nar',
+              logo: 'nar-logo.png',
               address: '0xCa6e313C535B6C355bEeAae296B58a194C3D6044',
               symbol: 'mooNarwhalTHUGS-NAR',
               abi: config.erc20ABI,
@@ -150,6 +153,7 @@ class Store {
           tokens: [
             {
               id: 'bifi-maxi',
+              logo: 'bifi-logo.png',
               address: '0xf7069e41C57EcC5F122093811d8c75bdB5f7c14e',
               symbol: 'mooBIFI',
               abi: config.erc20ABI,
