8529d36209862445d751cae8558bd3993457ea55
siphon-finance <80924933+siphon-finance@users.noreply.github.com>
2021-05-25 19:46:35 -0400

    Update layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 import Header from '../header';
 import Footer from '../footer';
-import Pastures from '../pastures';
+//import Pastures from '../pastures';
 import { Container } from './style';
-import Sky from '../sky';
+//import Sky from '../sky';
 
 const Layout = ({ children }) => (
   <Container>
     <Header />
     {children}
     <Footer />
-    <Sky />
-    <Pastures />
+//    <Sky />
+//    <Pastures />
   </Container>
 );
 
