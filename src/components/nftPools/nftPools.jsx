e799fb763409f9c61c9aa07e6a0fd90eebc68834
akyo8 <imirisa8@gmail.com>
2021-06-10 02:47:08 +1000

    back button issue

--- src/components/nftPools/nftPools.jsx
+++ src/components/nftPools/nftPools.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { withStyles } from '@material-ui/core/styles';
-import { Typography, Button } from '@material-ui/core';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import { Typography, Button } from "@material-ui/core";
 
-import UnlockModal from '../unlock/unlockModal.jsx';
-import Store from '../../stores/store';
-import { colors } from '../../theme/theme';
-import config from '../../config';
+import UnlockModal from "../unlock/unlockModal.jsx";
+import Store from "../../stores/store";
+import { colors } from "../../theme/theme";
+import config from "../../config";
 
-import gifNeonYellow from '../../assets/NFT-dark-backdrop-with-neon-yellow.gif'
+import gifNeonYellow from "../../assets/NFT-dark-backdrop-with-neon-yellow.gif";
 
 import {
   JOIN_POOL,
@@ -18,256 +18,256 @@ import {
   GET_BALANCES,
   GET_BALANCES_RETURNED,
   NFT_POOL_RECEIVED,
-} from '../../constants/constants';
+} from "../../constants/constants";
 
 const styles = (theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    maxWidth: '600px',
-    width: '100%',
-    alignItems: 'center',
-    margin: '0 auto',
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "600px",
+    width: "100%",
+    alignItems: "center",
+    margin: "0 auto",
   },
   intro: {
-    width: '100%',
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    maxWidth: '600px',
+    width: "100%",
+    position: "relative",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    maxWidth: "600px",
   },
   introCenter: {
-    minWidth: '100%',
-    textAlign: 'center',
-    padding: '48px 0px',
+    minWidth: "100%",
+    textAlign: "center",
+    padding: "48px 0px",
   },
   investedContainer: {
-    display: 'flex',
+    display: "flex",
     flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '12px',
-    minWidth: '100%',
-    [theme.breakpoints.up('md')]: {
-      minWidth: '800px',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: "12px",
+    minWidth: "100%",
+    [theme.breakpoints.up("md")]: {
+      minWidth: "800px",
     },
   },
   connectContainer: {
-    padding: '12px',
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-    maxWidth: '450px',
-    [theme.breakpoints.up('md')]: {
-      width: '450',
+    padding: "12px",
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    maxWidth: "450px",
+    [theme.breakpoints.up("md")]: {
+      width: "450",
     },
   },
   buttonText: {
-    fontWeight: '700',
-    color: 'white',
+    fontWeight: "700",
+    color: "white",
   },
   disclaimer: {
-    padding: '15px 40px',
-    background: 'rgba(80, 79, 105, 0.4)',
-    borderRadius: '5px',
-    marginBottom: '2rem',
+    padding: "15px 40px",
+    background: "rgba(80, 79, 105, 0.4)",
+    borderRadius: "5px",
+    marginBottom: "2rem",
     fontWeight: 600,
-    color: 'white',
-    letterSpacing: '-0.02em',
+    color: "white",
+    letterSpacing: "-0.02em",
   },
   nftPools: {
-    width: '100%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
   },
   nftPoolContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '260px',
-    padding: '2rem 3rem',
-    marginBottom: '2rem',
-    borderRadius: '5px',
-    background: '#504F69',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "260px",
+    padding: "2rem 3rem",
+    marginBottom: "2rem",
+    borderRadius: "5px",
+    background: "#504F69",
   },
   title: {
-    width: '100%',
+    width: "100%",
     color: colors.darkGray,
-    minWidth: '100%',
-    marginLeft: '20px',
+    minWidth: "100%",
+    marginLeft: "20px",
   },
   poolName: {
-    paddingBottom: '.2rem',
-    color: 'white',
+    paddingBottom: ".2rem",
+    color: "white",
   },
   poolLogo: {
-    width: '80px',
-    margin: '0 auto 16px',
+    width: "80px",
+    margin: "0 auto 16px",
   },
   poolBrief: {
-    color: '#A19C98',
-    marginBottom: '22px',
-    textDecoration: 'none',
-    '&:hover': { textDecoration: 'underline' },
+    color: "#A19C98",
+    marginBottom: "22px",
+    textDecoration: "none",
+    "&:hover": { textDecoration: "underline" },
   },
   contractLabel: {
-    color: '#DEDDFF',
-    paddingBottom: '20px',
+    color: "#DEDDFF",
+    paddingBottom: "20px",
   },
   contractAddress: {
-    color: '#53AEA3',
-    textDecoration: 'none',
-    '&:hover': { textDecoration: 'underline' },
+    color: "#53AEA3",
+    textDecoration: "none",
+    "&:hover": { textDecoration: "underline" },
   },
   actionButton: {
-    color: '#fff',
-    borderColor: '#000',
-    background: '#000',
-    fontWeight: '900',
-    padding: '.1rem 2rem',
-    '&:hover': {
-      background: '#5A8F69',
+    color: "#fff",
+    borderColor: "#000",
+    background: "#000",
+    fontWeight: "900",
+    padding: ".1rem 2rem",
+    "&:hover": {
+      background: "#5A8F69",
     },
   },
   overview: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    padding: '28px 30px',
-    borderRadius: '5px',
-    marginTop: '40px',
-    width: '100%',
-    background: '#504F69',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    padding: "28px 30px",
+    borderRadius: "5px",
+    marginTop: "40px",
+    width: "100%",
+    background: "#504F69",
   },
   overviewField: {
-    display: 'flex',
-    flexDirection: 'column',
-    margin: '0 5px',
+    display: "flex",
+    flexDirection: "column",
+    margin: "0 5px",
   },
   overviewTitle: {
-    color: '#DEDDFF',
+    color: "#DEDDFF",
   },
   overviewValue: {
-    color: 'white',
+    color: "white",
   },
   actions: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexWrap: 'wrap',
-    background: '#504F69',
-    padding: '28px 30px',
-    borderRadius: '5px',
-    marginTop: '40px',
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexWrap: "wrap",
+    background: "#504F69",
+    padding: "28px 30px",
+    borderRadius: "5px",
+    marginTop: "40px",
   },
   actionContainer: {
-    minWidth: 'calc(50% - 40px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '20px',
+    minWidth: "calc(50% - 40px)",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "20px",
   },
   actionButton: {
-    color: '#fff',
-    borderColor: '#000',
-    background: '#000',
-    fontWeight: '900',
-    padding: '.1rem 2rem',
-    '&:hover': {
-      background: '#5A8F69',
+    color: "#fff",
+    borderColor: "#000",
+    background: "#000",
+    fontWeight: "900",
+    padding: ".1rem 2rem",
+    "&:hover": {
+      background: "#5A8F69",
     },
   },
   primaryButton: {
-    color: '#fff',
-    borderColor: '#000',
-    background: '#000',
-    fontWeight: '900',
-    padding: '.1rem 2rem',
-    minHeight: '4rem',
-    minWidth: '14rem',
-    borderRadius: '5px',
-    '&:hover': {
-      background: '#5A8F69',
+    color: "#fff",
+    borderColor: "#000",
+    background: "#000",
+    fontWeight: "900",
+    padding: ".1rem 2rem",
+    minHeight: "4rem",
+    minWidth: "14rem",
+    borderRadius: "5px",
+    "&:hover": {
+      background: "#5A8F69",
     },
   },
   buttonText: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
   valContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
   },
   actionInput: {
-    padding: '0px 0px 12px 0px',
-    '& input': {
-      fontSize: '1.2rem',
-      borderBottom: '1px solid #DEDDFF',
-      color: 'white',
+    padding: "0px 0px 12px 0px",
+    "& input": {
+      fontSize: "1.2rem",
+      borderBottom: "1px solid #DEDDFF",
+      color: "white",
     },
   },
   inputAdornment: {
-    fontWeight: '600',
-    fontSize: '1.5rem',
-    color: '#DEDDFF',
+    fontWeight: "600",
+    fontSize: "1.5rem",
+    color: "#DEDDFF",
   },
   assetIcon: {
-    display: 'inline-block',
-    verticalAlign: 'middle',
-    borderRadius: '25px',
-    background: '#dedede',
-    height: '30px',
-    width: '30px',
-    textAlign: 'center',
-    marginRight: '16px',
+    display: "inline-block",
+    verticalAlign: "middle",
+    borderRadius: "25px",
+    background: "#dedede",
+    height: "30px",
+    width: "30px",
+    textAlign: "center",
+    marginRight: "16px",
   },
   balances: {
-    width: '100%',
-    textAlign: 'right',
-    paddingRight: '20px',
-    cursor: 'pointer',
-    color: '#DEDDFF',
+    width: "100%",
+    textAlign: "right",
+    paddingRight: "20px",
+    cursor: "pointer",
+    color: "#DEDDFF",
   },
   stakeButtons: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'space-around',
-    align: 'center',
-    marginTop: '2rem',
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-around",
+    align: "center",
+    marginTop: "2rem",
   },
   requirement: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
   },
   check: {
-    paddingTop: '6px',
+    paddingTop: "6px",
   },
   voteLockMessage: {
-    margin: '20px',
+    margin: "20px",
   },
   title: {
-    color: 'white',
-    marginBottom: '2rem',
-    width: '100%',
-    textAlign: 'center',
+    color: "white",
+    marginBottom: "2rem",
+    width: "100%",
+    textAlign: "center",
   },
   nftGif: {
-    width: '15em'
+    width: "15em",
   },
   nftGifContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '260px',
-    padding: '2rem 3rem',
-    borderRadius: '5px',
-    background: '#504F69',
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "260px",
+    padding: "2rem 3rem",
+    borderRadius: "5px",
+    background: "#504F69",
   },
 });
 
@@ -279,8 +279,8 @@ class NftPools extends Component {
   constructor(props) {
     super();
 
-    const account = store.getStore('account');
-    const nftPools = store.getStore('nftPools');
+    const account = store.getStore("account");
+    const nftPools = store.getStore("nftPools");
 
     this.state = {
       nftPools: nftPools,
@@ -304,7 +304,7 @@ class NftPools extends Component {
   }
 
   nftPoolsReceived = () => {
-    const nftPools = store.getStore('nftPools');
+    const nftPools = store.getStore("nftPools");
     this.setState({ nftPools: nftPools });
   };
 
@@ -318,7 +318,7 @@ class NftPools extends Component {
 
     return (
       <div className={classes.root}>
-        <Typography variant={'h5'} className={classes.disclaimer}>
+        <Typography variant={"h5"} className={classes.disclaimer}>
           This project is in Beta. Audit still in progress DYOR.
         </Typography>
         <div className={classes.nftPools}>
@@ -337,63 +337,61 @@ class NftPools extends Component {
     });
   };
 
-
   renderCurrentPool = () => {
-    const nftPools = store.getStore('nftPools');
+    const nftPools = store.getStore("nftPools");
     const { currentPool, showGif, gif } = this.state;
     const { classes } = this.props;
 
-    console.log('========', currentPool);
-
     return (
       <div className={classes.root}>
-        {' '}
+        {" "}
         <div className={classes.intro}>
           <Button
             className={classes.actionButton}
             onClick={() => {
-              this.props.history.push('/staking');
+              this.props.history.push("/nft");
+              this.setState({ currentPool: null });
             }}
           >
             Back
           </Button>
         </div>
-
-        {showGif && <div className={classes.nftGifContainer}>
-            <img className={classes.nftGif} src={gif} alt=""/>
-        </div>}
-
+        {showGif && (
+          <div className={classes.nftGifContainer}>
+            <img className={classes.nftGif} src={gif} alt="" />
+          </div>
+        )}
         <div className={classes.overview}>
           <div className={classes.overviewLogo}>
             <img
               alt=""
-              src={require('../../assets/' + nftPools.logo)}
+              src={require("../../assets/" + nftPools.logo)}
               height="48px"
             />
           </div>
 
           <div className={classes.overviewField}>
-            <Typography variant={'h3'} className={classes.overviewValue}>
+            <Typography variant={"h3"} className={classes.overviewValue}>
               0
             </Typography>
-            <Typography variant={'h4'} className={classes.overviewTitle}>
+            <Typography variant={"h4"} className={classes.overviewTitle}>
               Total Amount Paid
             </Typography>
           </div>
           <div className={classes.overviewField}>
-            <Typography variant={'h3'} className={classes.overviewValue}>
+            <Typography variant={"h3"} className={classes.overviewValue}>
               0
             </Typography>
-            <Typography variant={'h4'} className={classes.overviewTitle}>
+            <Typography variant={"h4"} className={classes.overviewTitle}>
               Total Per NFT
             </Typography>
           </div>
 
           <div className={classes.overviewField}>
-            <Typography variant={'h3'} className={classes.overviewValue}>
+            <Typography variant={"h3"} className={classes.overviewValue}>
               0
             </Typography>
-            <Typography variant={'h4'} className={classes.overviewTitle}>
+            <Typography variant={"h4"} className={classes.overviewTitle}>
               Last siphon payment
             </Typography>
           </div>
@@ -402,33 +400,33 @@ class NftPools extends Component {
           <div className={classes.overviewLogo}>
             <img
               alt=""
-              src={require('../../assets/' + nftPools.logo)}
+              src={require("../../assets/" + nftPools.logo)}
               height="48px"
             />
           </div>
 
           <div className={classes.overviewField}>
-            <Typography variant={'h3'} className={classes.overviewValue}>
+            <Typography variant={"h3"} className={classes.overviewValue}>
               {nftPools.investCap}
             </Typography>
-            <Typography variant={'h4'} className={classes.overviewTitle}>
+            <Typography variant={"h4"} className={classes.overviewTitle}>
               Invest Cap
             </Typography>
           </div>
           <div className={classes.overviewField}>
-            <Typography variant={'h3'} className={classes.overviewValue}>
+            <Typography variant={"h3"} className={classes.overviewValue}>
               {currentPool.investorNumber}
             </Typography>
-            <Typography variant={'h4'} className={classes.overviewTitle}>
+            <Typography variant={"h4"} className={classes.overviewTitle}>
               Investor Number
             </Typography>
           </div>
 
           <div className={classes.overviewField}>
-            <Typography variant={'h3'} className={classes.overviewValue}>
+            <Typography variant={"h3"} className={classes.overviewValue}>
               {nftPools.maxInvestorNumber}
             </Typography>
-            <Typography variant={'h4'} className={classes.overviewTitle}>
+            <Typography variant={"h4"} className={classes.overviewTitle}>
               Max Investor Number
             </Typography>
           </div>
@@ -443,7 +441,7 @@ class NftPools extends Component {
                   content: {
                     asset: config.nftABI,
                     sphnAsset: config.sphnABI,
-                    poolID: currentPool ? currentPool.poolID : '',
+                    poolID: currentPool ? currentPool.poolID : "",
                   },
                 })
               }
@@ -459,7 +457,7 @@ class NftPools extends Component {
                   type: REMOVE_POOL,
                   content: {
                     asset: config.nftABI,
-                    poolID: currentPool ? currentPool.poolID : '',
+                    poolID: currentPool ? currentPool.poolID : "",
                   },
                 })
               }
@@ -473,17 +471,17 @@ class NftPools extends Component {
   };
 
   renderNftPool = (nftPool) => {
-    const nftPools = store.getStore('nftPools');
+    const nftPools = store.getStore("nftPools");
     const { classes } = this.props;
 
     let address = null;
-    let addy = '';
-    
+    let addy = "";
+
     if (nftPools) {
       addy = nftPools.nftAddress;
       address =
         addy.substring(0, 6) +
-        '...' +
+        "..." +
         addy.substring(addy.length - 4, addy.length);
     }
 
@@ -492,7 +490,7 @@ class NftPools extends Component {
         <img
           alt={nftPool.poolID}
           className={classes.poolLogo}
-          src={require('../../assets/' + nftPools.logo)}
+          src={require("../../assets/" + nftPools.logo)}
         />
         <Typography variant="h3" className={classes.poolName}>
           {nftPool.poolName}
@@ -523,7 +521,7 @@ class NftPools extends Component {
         <Button
           className={classes.actionButton}
           onClick={() => {
-            this.handleOpenButton(nftPool)
+            this.handleOpenButton(nftPool);
           }}
         >
           Open
@@ -533,15 +531,15 @@ class NftPools extends Component {
   };
 
   handleOpenButton = (nftPool) => {
-    console.log(nftPool)
+    console.log(nftPool);
 
     if (nftPool.poolName === "Atos") {
-      this.setState({showGif: true, gif: gifNeonYellow});
-    } else if (nftPool.poolName === "Johnny"){
-      this.setState({showGif: true});
+      this.setState({ showGif: true, gif: gifNeonYellow });
+    } else if (nftPool.poolName === "Johnny") {
+      this.setState({ showGif: true });
     }
-    this.navigateNft(nftPool)
-  }
+    this.navigateNft(nftPool);
+  };
 
   navigateNft = (nftPool) => {
     this.setState({ currentPool: nftPool });
